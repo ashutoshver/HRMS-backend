a6935160dfb7a3c713230658fd8ce00bf0f78d51
cms-agbe <cms@agbeindia.com>
2026-02-14 21:57:18 +0530

    cors issue resolved

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "HRMS API is running" });
+});
 app.use("/api/v1/employees", employeeRouter);
 app.use("/api/v1/attendance", attendanceRouter);
 app.use("/api/v1/departments", departmentRouter);
